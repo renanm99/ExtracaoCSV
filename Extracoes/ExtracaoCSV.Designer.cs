0b04077853ed4149c16da8a31a2b2298ea222c29
R. Oliveira Machado <r.oliveira.machado@avanade.com>
2019-05-07 12:28:40 -0300

    ferramenta

---
+++ Extracoes/ExtracaoCSV.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Extracoes
+{
+    partial class ExtracaoCSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(944, 482);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(13, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Processar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Quantidade de linhas por arquivo";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 142);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(259, 26);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(950, 488);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(968, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(301, 485);
+            this.panel2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(183, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Abrir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(13, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(268, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Pasta de Entrega";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 353);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(278, 24);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "CSV em Inglês , *(Apenas Vígula)*";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // ExtracaoCSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1281, 512);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ExtracaoCSV";
+            this.Text = "ExtracaoCSV";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ Extracoes/ExtracaoCSV.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Extracoes
+{
+    public partial class ExtracaoCSV : Form
+    {
+        public ExtracaoCSV()
+        {
+            InitializeComponent();
+        }
+
+        private int count { get; set; }
+        private string div { get; set; }
+
+        public void GerarCSV()
+        {
+            SqlConnection sqlConn = new SqlConnection("data source=srvlogp02; user id=prconsul;password=consulta; initial catalog=BDFICH; application name=SS");
+            try
+            {
+                sqlConn.Open();
+
+                SqlCommand sqlComm = new SqlCommand();
+                sqlComm.CommandText = richTextBox1.Text;
+                sqlComm.Connection = sqlConn;
+                sqlComm.CommandTimeout = 0;
+
+                using (SqlDataReader reader = sqlComm.ExecuteReader())
+                {
+                    int arquivo = 0;
+                    if (reader.HasRows)
+                    {
+                        arquivo += 1;
+                        string filename = textBox1.Text + " - ";
+                        string filecsv = textBox3.Text + "\\" + filename + arquivo + ".csv";
+
+                        while (reader.Read())
+                        {
+                            //if (count == numericUpDown1.Value)
+                            //{
+                            //    this.count = 0;
+                            //    arquivo += 1;
+                            //    filecsv = filename + arquivo + ".csv";
+
+                            //    if (this.count == 0)
+                            //    {
+                            //        gerarCabecalho(filecsv, reader);
+                            //    }
+
+                            //    gerarCorpo(filecsv, reader);
+                            //    continue;
+                            //}
+                            //else
+                            //{
+                            //    if (this.count == 0)
+                            //    {
+                            //        gerarCabecalho(filecsv, reader);
+                            //    }
+
+                            //    gerarCorpo(filecsv, reader);
+                            //}
+
+                            if (count == numericUpDown1.Value)
+                            {
+                                this.count = 0;
+                                arquivo += 1;
+                                filecsv = textBox3.Text + "\\" + filename + arquivo + ".csv";
+                            }
+
+                            if (this.count == 0)
+                            {
+                                gerarCabecalho(filecsv, reader);
+                            }
+                            gerarCorpo(filecsv, reader);
+                        }
+                    }
+                    count = 0;
+                    MessageBox.Show("Processo foi finalizado.\nForam gerados " + arquivo + " arquivos.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro");
+            }
+            finally
+            {
+                sqlConn.Close();
+                sqlConn.Dispose();
+            }
+        }
+
+        public bool verificaCampos()
+        {
+            if (richTextBox1.Text != "" || textBox1.Text != "" || numericUpDown1.Value > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (verificaCampos())
+            {
+                this.count = 0;
+                this.div = ";";
+                if (checkBox1.Checked) { this.div = ","; }
+                GerarCSV();
+            }
+            else
+            {
+                MessageBox.Show("Corrija os campos!");
+            }
+        }
+
+        private void gerarCabecalho(string filecsv, SqlDataReader reader)
+        {
+            string header = "";
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                header += String.Format("{0}", reader.GetName(i));
+                if (i == reader.FieldCount - 1)
+                {
+                    header += Environment.NewLine;
+                    File.WriteAllText(filecsv, header);
+                    continue;
+                }
+                header += this.div;
+            }
+        }
+
+        private void gerarCorpo(string filecsv, SqlDataReader reader)
+        {
+            string body = "";
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                body += String.Format("{0}", reader.GetValue(i));
+                if (i == reader.FieldCount - 1)
+                {
+                    body += Environment.NewLine;
+                    File.AppendAllText(filecsv, body);
+                    body = "";
+                    this.count++;
+                    continue;
+                }
+                body += this.div;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+                button1.Enabled = true;
+            }
+        }
+    }
+
+}
